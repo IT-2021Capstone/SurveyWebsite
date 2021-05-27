7dda680efab5f1e9c6b401ec94800547d458562c
DARRIEN GONZALEZ <DARRIEN.GONZALEZ@student.centralia.edu>
2021-05-27 13:07:18 -0700

    Added Question, QuestionLIstBase, QuestionOptions, and QuestionOptionListBase to work with blazor, got adding new questions working

---
+++ DataAccessLibrary/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyWebsite.DataAccessLibrary.Models
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public string Questions { get; set; }
+        public string Type { get; set; }
+        public IEnumerable<QuestionOption> QOpts { get; set; }
+    }
+}

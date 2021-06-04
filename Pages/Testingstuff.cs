c097b27307425d8b96e24fa5b162202f6bff4af0
macmurp <macmurp@gmail.com>
2021-06-03 17:32:19 -0700

    testing adding of questions by type via switch case

--- Pages/Testingstuff.cs
+++ Pages/Testingstuff.cs
@@ -17,12 +17,12 @@ namespace SurveyWebsite.Pages
         }
 
 
-        public void SendQuestion(int id, string text, int qtype )
+        public void TestQuestion()
         {
             Question[] q;
-            var sureveyID = id;
-            var questionText = text;
-            var questionType = qtype;
+            var sureveyID = 3;
+            var questionText = "This is from the website";
+            var questionType = 1;
             SqlParameter param1 = new SqlParameter("@surveyID", sureveyID);
             SqlParameter param2 = new SqlParameter("@questionText", questionText);
             SqlParameter param3 = new SqlParameter("@questionType", questionType);
@@ -31,6 +31,41 @@ namespace SurveyWebsite.Pages
                 .ToArray();
 
         }
-        
+
+        public void SendQuestion(int surveyid, string questiontext, int questiontype)
+        {
+            Question[] q;
+            int _surveyID = surveyid;
+            int _questiontype;
+            string _questiontext;
+            switch (questiontype)
+            {
+                //switch case may not be necessary now, but seems like a good practice if we
+                //want to add more question types in the future
+                case 1:
+                    //open ended question type being submitted
+                    _questiontype = 1;
+                    _questiontext = questiontext;
+                    //execute addnonmutiplequestion
+                    q = _context.Questions
+                        .FromSqlRaw("EXECUTE AddNonMutipleQuestion @surveyID, @questionText, @questionType",
+                        _surveyID, _questiontext, _questiontype)
+                        .ToArray();
+                    break;
+                case 2:
+                    //true false question type being submitted
+                    _questiontype = 2;
+                    _questiontext = questiontext;
+                    //execute addmutiplequestion
+                    break;
+                case 3:
+                    //multiple choice question type being submitted
+                    _questiontype = 3;
+                    _questiontext = questiontext;
+                    //execute addmutiplequestion
+                    break;
+            }
+        }
+
     }
 }

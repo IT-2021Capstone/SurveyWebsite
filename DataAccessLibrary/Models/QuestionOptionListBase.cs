9b03dd27975265aa8fa190dac15730cc9186d8b7
DARRIEN GONZALEZ <DARRIEN.GONZALEZ@student.centralia.edu>
2021-06-10 10:47:43 -0700

    Questions can be added still having issues implimneting QuestionOptions to add question to the surveys

--- DataAccessLibrary/Models/QuestionOptionListBase.cs
+++ DataAccessLibrary/Models/QuestionOptionListBase.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using SurveyWebsite.DataAccessLibrary.Models;
 
 namespace SurveyWebsite.DataAccessLibrary.Models
 {
     public class QuestionOptionListBase : ComponentBase
     {
-
-        
-        
+        public static IEnumerable<QuestionOption> QuestionOptions { get; set;}
+        protected override Task OnInitializedAsync()
+        {
+            
+            return base.OnInitializedAsync();
+        }
     }
 }
